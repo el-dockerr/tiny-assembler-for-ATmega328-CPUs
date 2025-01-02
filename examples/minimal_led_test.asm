57a8a910c21d475bb61aca5991ac45f757afb1ee
El Dockerr <kalski.swen@gmail.com>
2025-01-02 18:25:08 +0100

    fix new file

--- examples/minimal_led_test.asm
+++ examples/minimal_led_test.asm
@@ -5,9 +5,10 @@
 ; Test program counter and execution flow
 ; Author: Swen Kalski
 
+START:
     LDI R16, 0x20       ; Set bit 5 (0b00100000)
     OUT 0x24, R16       ; DDRB - configure Pin 5 as output
     OUT 0x25, R16       ; PORTB - LED ON
     CLR R17             ; Clear R17
     OUT 0x25, R17       ; PORTB - LED OFF
-    JMP 0x0000          ; Jump to start
+    JMP START           ; Jump to start
